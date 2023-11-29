9ce7b7e2a3db394822932be77d9f11efef69dfaf
DanielHenryL <lagunasdaniel001@gmail.com>
2023-11-29 12:28:37 -0500

    Gregando el componente Sidebar, ver los atrbutos routerLink, routerLinkActive y routerLinkActveOptions

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,29 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContacPageComponent } from './pages/contac-page/contac-page.component';
 
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    SidebarComponent,
+    ContacPageComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    SidebarComponent,
+    ContacPageComponent,
   ]
 })
 export class SharedModule { }
