5dfe34055d7639a66d799d998fbf61a024c6a248
DanielHenryL <lagunasdaniel001@gmail.com>
2023-11-30 12:14:19 -0500

    Refactorizando el codigo de subcribe, mediante switchMap

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  constructor( private router:Router, private activatedRoute: ActivatedRoute, private countriesService:CountriesService ){}
+
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.countriesService.searchCapitalByAlphaCode(id) )
+      )
+      .subscribe( country => {
+        if ( !country ) {
+          return this.router.navigateByUrl('');
+        }
+        console.log( 'Tenemos un pais' );
+        return;
+      })
+  }
 
 }
