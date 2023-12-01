6b92241b64d7310d9f04cce71014d94b4c2f9db0
DanielHenryL <lagunasdaniel001@gmail.com>
2023-12-01 14:38:44 -0500

    refactorizando en nuestro servicio

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interface/country.interface';
 
 @Injectable({providedIn: 'root'})
@@ -11,26 +11,33 @@ export class CountriesService {
   constructor(private http: HttpClient) { }
 
   searchCapitalByAlphaCode( code:string ):Observable<Country|null>{
-    return this.http.get<Country[]>(`${ this.apiUrl }/alpha/${ code }`)
+    const url:string = `${ this.apiUrl }/alpha/${ code }`
+    return this.http.get<Country[]>( url )
       .pipe(
         map( countries => countries.length > 0 ? countries[0]: null),
         catchError( error => of(null))
       )
   }
-
+  private getCountriesRequest( url:string ):Observable<Country[]>{
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( error => of([])),
+        delay(2000)
+      )
+  }
   searchCapital( term:string ):Observable<Country[]>{
-    return this.http.get<Country[]>(`${ this.apiUrl }/capital/${ term }`)
-            .pipe( catchError( error => of([])))
+    const url:string = `${ this.apiUrl }/capital/${ term }`
+    return this.getCountriesRequest(url)
   }
 
   searchCountry( term:string ):Observable<Country[]>{
-    return this.http.get<Country[]>(`${ this.apiUrl }/name/${ term }`)
-            .pipe( catchError( error => of([])))
+    const url:string = `${ this.apiUrl }/name/${ term }`
+    return this.getCountriesRequest( url )
   }
 
   searchRegion( region:string ):Observable<Country[]>{
-    return this.http.get<Country[]>(`${ this.apiUrl }/region/${ region }`)
-            .pipe( catchError( error => of([])))
+    const url:string = `${ this.apiUrl }/region/${ region }`
+    return this.getCountriesRequest( url )
   }
 
 }
